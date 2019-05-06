30ecd1dd0c63cd34a90faf4f478125ce46f88ec9
Marius Margowski <mariusmargowski89@gmail.com>
2019-05-06 18:27:11 +0200

    Added dataset

---
+++ Convolutional Neural Networks (CNN)/cnn.py
@@ -0,0 +1,12 @@
+# Convolutional Neural Network
+# Predicting if a picture is a cat or a dog.
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
