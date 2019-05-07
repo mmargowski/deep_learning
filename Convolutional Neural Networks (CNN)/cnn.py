63a95a0794f3f8f53c3b0a5d5a68c37c7d3e2368
Marius Margowski <mariusmargowski89@gmail.com>
2019-05-07 19:25:50 +0200

    Added code for single picture prediction

--- Convolutional Neural Networks (CNN)/cnn.py
+++ Convolutional Neural Networks (CNN)/cnn.py
@@ -33,7 +33,6 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-
 # Step 3 - Flattening
 classifier.add(Flatten())
 
@@ -67,6 +66,20 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 25,
+                         epochs = 10,
                          validation_data = test_set,
                          validation_steps = 2000)
+
+# --- Making a new prediction ---
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
