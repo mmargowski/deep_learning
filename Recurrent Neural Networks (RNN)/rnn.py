9cce17fb406d219c8998435aac483b16dbe7ee8b
Marius Margowski <mariusmargowski89@gmail.com>
2019-05-11 09:04:57 +0200

    Added overview graphic + RNN setup

---
+++ Recurrent Neural Networks (RNN)/rnn.py
@@ -0,0 +1,10 @@
+# Recurrent Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
