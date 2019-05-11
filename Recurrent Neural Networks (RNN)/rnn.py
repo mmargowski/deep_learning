c90c075a0a1547c32e907a54c47fc73d86a6ed6c
Marius Margowski <mariusmargowski89@gmail.com>
2019-05-11 11:09:34 +0200

    Added recurrent neural network

--- Recurrent Neural Networks (RNN)/rnn.py
+++ Recurrent Neural Networks (RNN)/rnn.py
@@ -1,4 +1,5 @@
 # Recurrent Neural Network
+# Trying to predict the google open stock price
 
 # Installing Theano
 # pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
@@ -8,3 +9,70 @@
 
 # Installing Keras
 # pip install --upgrade keras
+
+# --- Data Preprocessing ---
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output(Remember 3 months)
+X_train = []
+y_train = []
+for i in range(60, len(training_set_scaled)):
+    X_train.append(training_set_scaled[i-60:i,0])
+    y_train.append(training_set_scaled[i,0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1) )
+
+
+# --- Building the RNN ---
+
+# Importing the libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initialize the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some dropout regularization
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding the second LSTM layer and some dropout regularization
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding the third LSTM layer and some dropout regularization
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding the third LSTM layer and some dropout regularization
+regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compile the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the training set
+regressor.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+
+# --- Making the prediction + visualising the results ---
